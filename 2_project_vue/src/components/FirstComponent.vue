a63c6a4c457be700c8223233e8ad50bd44a5f947
joaokeuwe <joaov2872@hotmail.com>
2022-09-22 02:44:48 -0300

    feat: :bug: Aprendendo a componentizar elementos e fazendo a renderização deles

--- 2_project_vue/src/components/FirstComponent.vue
+++ 2_project_vue/src/components/FirstComponent.vue
@@ -1,9 +1,33 @@
 <template>
-  <h1>olá vuee</h1>
+  <div>
+    <h1>olá vue</h1>
+    <p>Meu nome é {{ nome }}</p>
+    <form >
+      <p>{{input}}</p>
+    </form>
+    <input
+          type="text"
+          v-model="input"
+          placeholder="Digite o seu nome"
+        />
+    <input type="submit" v-on:click="submit" />
+  </div>
 </template>
 
 <script>
 export default {
-  name: "App",
+  name: "FirstComponent",
+  data() {
+    return {
+      nome: "",
+      input: ""
+    };
+  },
+  methods: {
+    submit(e) {
+      e.preventDefault();
+      this.nome = this.input;
+    }
+  },
 };
 </script>
