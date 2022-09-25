ae677ec0be9cf0f9686f41c8be72400311e97266
joaokeuwe <joaov2872@hotmail.com>
2022-09-25 18:57:19 -0300

    feat: :fire: Criando componente Info para ser informações do componente Pessoa

---
+++ 2_project_vue/src/components/Info.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <p>Estou trabalhando no momento</p>
+    <p>Estou utilizando essas linguagens</p>
+
+    <ul>
+      <li>MYSQL</li>
+      <li>PHP</li>
+      <li>REACT</li>
+    </ul>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Info",
+  data() {},
+};
+</script>
