990dc9862764e894b1b3b939f0c44ff2c3886a57
joaokeuwe <joaov2872@hotmail.com>
2022-09-25 15:35:55 -0300

    feat: :art: Aprendendo sobre diretivas

---
+++ 2_project_vue/src/components/info.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+     <p>
+        Estou trabalhando
+     </p>
+     <p>Estou utilizando essas linguagens</p>
+    </div>
+</template>
+    
+<script>
+import { initCustomFormatter } from 'vue';
+
+    export default {
+        name:initCustomFormatter,
+        data() {
+            return {
+                trabalhando: true
+            }
+        }
+    }
+</script>
