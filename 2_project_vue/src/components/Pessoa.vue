527e822cd8d39fdffa786446a516d6a154a38ae3
joaokeuwe <joaov2872@hotmail.com>
2022-09-25 18:57:44 -0300

    feat: :fire: Criando componente Pessoa, com informações pessoais e renderizando componentes nele

---
+++ 2_project_vue/src/components/Pessoa.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h2>Esta é a pessoa {{ nome }}</h2>
+    <Info />
+    <Form />
+  </div>
+</template>
+
+<script>
+import Info from "./Info";
+import Form from "./Form.vue";
+
+export default {
+  name: 'Pessoa',
+  components: {
+    Info,
+    Form,
+  },
+  data() {
+    return {
+      nome: "keuwe",
+    };
+  },
+  
+};
+</script>
