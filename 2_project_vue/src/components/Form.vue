4f36a2c7f07a94d252f98e75dc5b21f402d1eb72
joaokeuwe <joaov2872@hotmail.com>
2022-09-25 18:56:47 -0300

    feat: :zap: Criando um elemento  filho na hierarquia de componentes, Form

---
+++ 2_project_vue/src/components/Form.vue
@@ -0,0 +1,19 @@
+<template>
+  <form action="">
+    <InputText />
+    <ButtonSubmit />
+  </form>
+</template>
+
+<script>
+import InputText from "./forms/InputText.vue";
+import ButtonSubmit from "./forms/submit.vue";
+export default {
+  name: "Form",
+  components: {
+    InputText,
+    ButtonSubmit,
+
+  },
+};
+</script>
