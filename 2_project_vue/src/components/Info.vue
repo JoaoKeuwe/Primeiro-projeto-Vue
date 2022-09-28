dc0b1851af0e477bb1adb2e01887b2c5fe818e8a
joaokeuwe <joaov2872@hotmail.com>
2022-09-27 23:26:43 -0300

    feat: :chart_with_upwards_trend: Importando o componenet de imagem já coma  logica de v-bind resumida

--- 2_project_vue/src/components/Info.vue
+++ 2_project_vue/src/components/Info.vue
@@ -10,18 +10,25 @@
     </ul>
 
     <p v-show="mostrar_email">Mande uma mensagem para {{ email }} </p>
+    <p>Para acessar meu porrtifólio  <a v-bind:href="meu_link" target="blank"> Basta clicar aqui</a></p>
+    <Picture />
     <p v-if="2<4"> test</p>
   </div>
 </template>
 
 <script>
+  import Picture from "./picture.vue"
 export default {
   name: "Info",
+  components: {
+    Picture
+  },
   data() {
     return {
       esta_trabalhando: true,
       mostrar_email: true,
-      email:'joaov2872@hotmail.com'
+      email:'joaov2872@hotmail.com',
+      meu_link: "https://github.com/JoaoKeuwe"
     }
   },
 };
