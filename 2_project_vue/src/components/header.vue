2b293d106612fccd85e175f2d2af0571487b2876
joaokeuwe <joaov2872@hotmail.com>
2022-09-28 00:55:34 -0300

    feat: :package: Criando componente Header

---
+++ 2_project_vue/src/components/header.vue
@@ -0,0 +1,33 @@
+<template>
+    <ul>
+        <li ><a href="#">Home</a></li>
+        <li><a href="#">Detalhes</a></li>
+        <li><a href="#">Sair</a></li>
+    </ul>
+</template>
+
+<script>
+export default {
+    name: 'Header'
+}
+</script>
+
+<style scoped>
+    ul {
+        display: flex;
+        list-style: none
+    }
+    li {
+        margin-right: 10px;
+    }
+    
+    a{
+        color : rgb(123, 26, 26);
+        text-align: none;
+        transition: .5s
+    }
+
+    a:hover {
+        color: turquoise
+    }
+</style>
