838a1e3cc04d2aabe9bc5ee691ba6c44ef6ac8ab
joaokeuwe <joaov2872@hotmail.com>
2022-09-27 23:26:54 -0300

    feat: :hammer: Componente de Imagem

---
+++ 2_project_vue/src/components/picture.vue
@@ -0,0 +1,15 @@
+<template>
+    <img :src='avatar' :alt='descricao'>
+</template>
+
+<script>
+export default {
+    name: 'Picture',
+    data() {
+        return {
+            avatar: '/img/logo.png',
+            descricao: "programador"
+        }
+    }
+}
+</script>
