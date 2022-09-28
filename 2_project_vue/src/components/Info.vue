693c36304f8879c4660a927d02899e826d04ec36
joaokeuwe <joaov2872@hotmail.com>
2022-09-27 23:01:18 -0300

    feat: :ambulance: usando diretiva v-show

--- 2_project_vue/src/components/Info.vue
+++ 2_project_vue/src/components/Info.vue
@@ -1,13 +1,16 @@
 <template>
   <div>
     <p v-if="esta_trabalhando">Estou trabalhando no momento</p>
-    <p>Estou utilizando essas linguagens</p>
+    <p v-else>Estou utilizando essas linguagens</p>
 
     <ul>
       <li>MYSQL</li>
       <li>PHP</li>
       <li>REACT</li>
     </ul>
+
+    <p v-show="mostrar_email">Mande uma mensagem para {{ email }} </p>
+    <p v-if="2<4"> test</p>
   </div>
 </template>
 
@@ -16,7 +19,9 @@ export default {
   name: "Info",
   data() {
     return {
-      esta_trabalhando: false
+      esta_trabalhando: true,
+      mostrar_email: true,
+      email:'joaov2872@hotmail.com'
     }
   },
 };
