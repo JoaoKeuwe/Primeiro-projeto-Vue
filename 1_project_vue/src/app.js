9781fae7cd9852985f0582882bb01cd3d20e4242
joaokeuwe <joaov2872@hotmail.com>
2022-11-11 21:40:10 -0300

    refactor:

--- 1_project_vue/src/app.js
+++ 1_project_vue/src/app.js
@@ -14,26 +14,3 @@ const myNameApp = {
 };
 
 Vue.createApp(myNameApp).mount("#app");
-
-
-// const MyNameApp = {
-//     data() {
-//       return {
-//         name: "",
-//         input_name: ""
-//       }
-//     },
-//     methods: {
-//       submitForm(e) {
-  
-//         e.preventDefault()
-  
-//         console.log(this.input_name);
-  
-//         this.name = this.input_name;
-  
-//       }
-//     }
-//   }
-  
-//   Vue.createApp(MyNameApp).mount('#app')
